d369d9e3c1d2fa5236889374da995925a900efa3
Rosa Naharro Vaillant <rosana50factoria@gmail.com>
2026-06-23 12:29:08 +0200

    feat: validations ISBN and FORMAT

--- src/main/java/com/femcoders/controller/BookController.java
+++ src/main/java/com/femcoders/controller/BookController.java
@@ -8,30 +8,34 @@ import com.femcoders.repository.BookRepository;
 import com.femcoders.repository.PublisherRepository;
 
 public class BookController {
-     private BookRepository bookRepository;
-     private AuthorRepository authorRepository;
-     private PublisherRepository publisherRepository;
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+    private PublisherRepository publisherRepository;
 
-     public BookController(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository){
+    public BookController(BookRepository bookRepository, AuthorRepository authorRepository,
+            PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
         this.publisherRepository = publisherRepository;
-     }
+    }
 
-    public void createBook(Book book){
-        if (!bookRepository.validateExistingIsbn(book.getIsbn())) {
-
-            Author author = authorRepository.validateExistingAuthor(book.getAuthor().getName());
-            Publisher publisher = publisherRepository.validateExistingPublisher(book.getPublisher().getName());
-
-            book.setAuthor(author);
-            book.setPublisher(publisher);
-
-            bookRepository.createBook(book);
+    public void createBook(Book book) {
+        if (!book.getIsbn().matches("\\d{13}")) {
+            System.out.println("Invalid ISBN. It must contain exactly 13 numeric digits.");
+            return;
         }
 
-        else {
+        if (bookRepository.validateExistingIsbn(book.getIsbn())) {
             System.out.println("This ISBN already exists. We can not add this book.");
+            return;
         }
-    }  
+
+        Author author = authorRepository.validateExistingAuthor(book.getAuthor().getName());
+        Publisher publisher = publisherRepository.validateExistingPublisher(book.getPublisher().getName());
+
+        book.setAuthor(author);
+        book.setPublisher(publisher);
+
+        bookRepository.createBook(book);
+    }
 }
